442c905f8e28ff1750b4257bc73a9d56955d49bf
ashutoshpmv <ashutoshpmv@gmail.com>
2025-08-22 19:17:54 +0530

    Add base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/my-portfolio/',   // 👈 IMPORTANT: must match your repo name
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
